cb7df2fb5574b8aec4df6f62eaf14d079a16f771
unknown <wesley.taylor303@gmail.com>
2020-04-20 15:35:09 -0400

    initial connection

--- Program.cs
+++ Program.cs
@@ -1,12 +1,33 @@
 ﻿using System;
+using Discord.WebSocket;
+using Discord;
+using System.Threading.Tasks;
+
 
 namespace OverwatchBot
 {
-    class Program
+    public class Program
     {
-        static void Main(string[] args)
+        private DiscordSocketClient _client;
+        public static void Main(string[] args)
+            => new Program().MainAsync().GetAwaiter().GetResult();
+
+        public async Task MainAsync()
         {
-            Console.WriteLine("Hello World!");
+            _client = new DiscordSocketClient();
+
+            _client.Log += Log;
+
+            await _client.LoginAsync(TokenType.Bot,"NzAxODcxODQ1NTc3ODUxMDAy.Xp32zg.nf-U9VfRrHtO92E3BRZPkZ7Q8Pc");
+            await _client.StartAsync();
+
+            await Task.Delay(-1);
+        }
+
+        private Task Log(LogMessage msg)
+        {
+            Console.WriteLine(msg.ToString());
+            return Task.CompletedTask;
         }
     }
 }
